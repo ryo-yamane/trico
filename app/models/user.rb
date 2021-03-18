7fd4b57289ca5e715d462c4c4e0e5d3f925e9618
ryo-yamane <78146211+ryo-yamane@users.noreply.github.com>
2021-03-19 01:41:22 +0900

    ログインユーザー名表示、ログアウトボタン

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+          validates :first_name
+          validates :last_name
+        end
+      
+        with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'カタカナでお願いします' } do
+          validates :first_name_kana
+          validates :last_name_kana
+        end
+
+        with_options presence: true do
+          validates :birthday
+          validates :nickname
+        end
+
+
+
 end
