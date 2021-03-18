f600c76f9570902fe66e6be7c2c0798a65fc7b9a
ryo-yamane <78146211+ryo-yamane@users.noreply.github.com>
2021-03-19 01:14:50 +0900

    登録画面ビュー整頓

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana,
+                                             :birthday, :salon_name, :address,:phone_number, :how_to_pay_id, :salon_consept_id,
+                                             :nearest_station, :rest_day_id])
+  end
+
+
 end
