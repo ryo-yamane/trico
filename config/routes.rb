c478b3f52a2b7a2a82ddbfa46f516ff18387e49a
ryo-yamane <78146211+ryo-yamane@users.noreply.github.com>
2021-03-18 17:50:53 +0900

    トップページとそれぞれのビュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,12 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-root to: 'home#index'
+root to: 'salons#index'
+
+  resources :salons, only: [:index] do
+
+    collection do
+     get 'choose_index'
+    end
+  end
 end
