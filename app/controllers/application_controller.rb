ee50169202eae210ae7a8b0e2dc2c6f41df4e9af
ryo-yamane <78146211+ryo-yamane@users.noreply.github.com>
2021-03-21 21:28:46 +0900

    オーナー情報登録完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
                                       keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana,
-                                             :birthday, :salon_name, :address,:phone_number, :how_to_pay_id, :salon_consept_id,
-                                             :nearest_station, :rest_day_id])
+                                             :birthday, :salon_name, :owner_first_name, :owner_last_name, :owner_first_name_kana, :owner_last_name_kana, :address,:phone_number, :how_to_pay_id, :salon_concept_id,
+                                             :sns_account, :nearest_station, :rest_day_id])
   end
 
 
