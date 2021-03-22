2dd24334fcb531cc620b7d805e81fa5844e92ab8
ryo-yamane <78146211+ryo-yamane@users.noreply.github.com>
2021-03-22 12:48:22 +0900

    SNS認証全体設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,17 +7,20 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions:     'users/sessions',
     passwords:    'users/passwords',
-    registrations: 'users/registrations' 
+    registrations: 'users/registrations', 
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
   }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
  root to: 'salons#index'
-
+  resources :users, only: :new
   resources :salons, only: [:index] do
 
     collection do
      get 'choose_index'
     end
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_093736) do
+ActiveRecord::Schema.define(version: 2021_03_21_131004) do
 
   create_table "owners", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,6 +34,15 @@ ActiveRecord::Schema.define(version: 2021_03_17_093736) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_owners_on_email", unique: true
     t.index ["reset_password_token"], name: "index_owners_on_reset_password_token", unique: true
+  end
+
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
