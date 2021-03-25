4f07ea6bda3288ba808216645c0fcb12efeacd7b
ryo-yamane <78146211+ryo-yamane@users.noreply.github.com>
2021-03-25 22:04:30 +0900

    ビューの大枠完成＆ログイン後のリダイレクトも完成

--- app/controllers/salons_controller.rb
+++ app/controllers/salons_controller.rb
@@ -1,5 +1,8 @@
 class SalonsController < ApplicationController
   def index
+    if user_signed_in?
+      redirect_to users_path
+    end
   end
 
   def choose_index
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -16,6 +16,7 @@ def authorization
 
   if @user.persisted? #ユーザー情報が登録済みなので、新規登録ではなくログイン処理を行う
      sign_in_and_redirect @user, event: :authentication
+     
   else #ユーザー情報が未登録なので、新規登録画面へ遷移する
      @sns_id = sns_info[:sns].id
      render template: 'users/registrations/new'
